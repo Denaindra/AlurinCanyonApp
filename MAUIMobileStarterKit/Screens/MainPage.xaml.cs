6479fa1f3c3c35007aabbd79fdef81a8bf97e7af
Gayan Perera <mgdperera@gmail.com>
2023-12-04 07:36:29 +0530

    add main page screen

--- MAUIMobileStarterKit/Screens/MainPage.xaml.cs
+++ MAUIMobileStarterKit/Screens/MainPage.xaml.cs
@@ -5,25 +5,15 @@ namespace MAUIMobileStarterKit.Screens;
 
 public partial class MainPage : ContentPage
 {
-	private readonly IUserDialogs userDialogs;
-    private readonly MainPageViewModels viewModels;
 
-    public MainPage(MainPageViewModels viewModel, IUserDialogs userDialogs)
-	{
-		InitializeComponent();
-		this.userDialogs = userDialogs;
-        this.viewModels = viewModel;
-	}
+    public MainPage()
+    {
+        InitializeComponent();
+    }
 
     protected override void OnAppearing()
     {
         base.OnAppearing();
     }
 
-   async void Button_Clicked_1(System.Object sender, System.EventArgs e)
-    {
-        viewModels.StartLoading();
-        await Task.Delay(3000);
-        viewModels.EndLoading();
-    }
 }
