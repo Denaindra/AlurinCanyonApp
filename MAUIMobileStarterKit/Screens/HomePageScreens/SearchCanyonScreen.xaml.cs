24f14e161782feffee87b806c2300723cbbed555
Gayan Perera <mgdperera@gmail.com>
2023-12-05 06:44:43 +0530

    add search bar

--- MAUIMobileStarterKit/Screens/HomePageScreens/SearchCanyonScreen.xaml.cs
+++ MAUIMobileStarterKit/Screens/HomePageScreens/SearchCanyonScreen.xaml.cs
@@ -6,4 +6,9 @@ public partial class SearchCanyonScreen : ContentView
 	{
 		InitializeComponent();
 	}
+
+    private void SearchButtonPressed(object sender, EventArgs e)
+    {
+
+    }
 }
--- MAUIMobileStarterKit/Screens/HomeScreen.xaml.cs
+++ MAUIMobileStarterKit/Screens/HomeScreen.xaml.cs
@@ -1,19 +1,26 @@
 ﻿using Controls.UserDialogs.Maui;
+using MAUIMobileStarterKit.Screens.HomePageScreens;
 using MAUIMobileStarterKit.ViewModels;
 
 namespace MAUIMobileStarterKit.Screens;
 
 public partial class HomeScreen : ContentPage
 {
-
+    
     public HomeScreen()
     {
         InitializeComponent();
     }
-
+  
     protected override void OnAppearing()
     {
         base.OnAppearing();
+        childLayout.Add(new SearchCanyonScreen());
     }
+    private void OnTapGestureRecognizerTapped(object sender, TappedEventArgs e)
+    {
+        var tappedParameter = (TappedEventArgs)e;
+        var parameter = Convert.ToInt32(tappedParameter.Parameter);
 
+    }
 }
