4ce3e56d531f188c1c794d922ee2e62427df082e
Gayan Perera <mgdperera@gmail.com>
2023-12-05 07:14:34 +0530

    edit home screen view

--- MAUIMobileStarterKit/Screens/HomeScreen.xaml.cs
+++ MAUIMobileStarterKit/Screens/HomeScreen.xaml.cs
@@ -15,7 +15,13 @@ public partial class HomeScreen : ContentPage
     protected override void OnAppearing()
     {
         base.OnAppearing();
-        childLayout.Add(new SearchCanyonScreen());
+       childLayout.Add(new SearchCanyonScreen());
+    }
+    protected override void OnSizeAllocated(double pageWidth, double pageHeight)
+    {
+        //base.OnSizeAllocated(pageWidth, pageHeight);
+        //const double aspectRatio = 1600 / 1441.0; // Aspect ratio of the original image
+        //backgroundImage.WidthRequest = Math.Max(pageHeight * aspectRatio, pageWidth);
     }
     private void OnTapGestureRecognizerTapped(object sender, TappedEventArgs e)
     {
