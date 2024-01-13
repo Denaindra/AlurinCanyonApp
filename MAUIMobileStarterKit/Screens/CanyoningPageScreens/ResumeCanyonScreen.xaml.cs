1ebe7fdfdfb33f0f5111f19a3de05ac5b9f3e4ce
Gayan Perera <mgdperera@gmail.com>
2024-01-13 19:25:14 +0530

    add resume items

--- MAUIMobileStarterKit/Screens/CanyoningPageScreens/ResumeCanyonScreen.xaml.cs
+++ MAUIMobileStarterKit/Screens/CanyoningPageScreens/ResumeCanyonScreen.xaml.cs
@@ -1,3 +1,5 @@
+using MAUIMobileStarterKit.Constant;
+
 namespace MAUIMobileStarterKit.Screens.CanyoningPageScreens;
 
 public partial class ResumeCanyonScreen : ContentView
@@ -5,5 +7,12 @@ public partial class ResumeCanyonScreen : ContentView
 	public ResumeCanyonScreen()
 	{
 		InitializeComponent();
-	}
+        SetupUI();
+
+    }
+
+    private void SetupUI()
+    {
+        scrollView.HeightRequest = 18000;//Constans.DeviceHeight;
+    }
 }
